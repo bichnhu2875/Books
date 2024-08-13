ddeeb57573f49a3eff769c83a1d557ad8bb5d101
nhulop6c@gmail.com <vonhu2875@gmail.com>
2024-08-13 10:05:23 +0700

    library
    Create Library

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-function changeDisplayRegister() {
-    var reg = document.getElementById("regId");
-    reg.style.display = "flex";
-    var login = document.getElementById("loginId");
-    login.style.display = "none";
-}
 
-function changeDisplayLogin() {
-    var login = document.getElementById("loginId");
-    login.style.display = "flex";
-    var reg = document.getElementById("regId");
-    reg.style.display = "none";
-}
